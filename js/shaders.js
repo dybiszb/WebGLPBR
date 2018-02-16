b5dc0dfc6aa4d23b7db6acb18c2eab8d526eef74
dybiszb <bartlomiej.dybisz@gmail.com>
2018-02-16 20:51:45 +0100

    Background color change + samplers in shaders.

--- js/shaders.js
+++ js/shaders.js
@@ -7,16 +7,21 @@ const pbrVS =
 // -------------------------------------------------
 
 attribute vec4 a_position;
+attribute vec2 a_texcoord;
 
 uniform mat4 u_model;
 uniform mat4 u_view;
 uniform mat4 u_projection;
+
+varying vec2 v_texcoord;
 
 void main() {
     gl_Position = u_projection *
                   u_view       *
                   u_model      *
                   a_position;
+
+    v_texcoord  = a_texcoord;
 }
 `;
 
@@ -32,7 +37,14 @@ precision mediump float;
 
 uniform vec3 u_color;
 
+uniform sampler2D u_albedo;
+uniform sampler2D u_metallic;
+uniform sampler2D u_normal;
+uniform sampler2D u_roughness;
+
+varying vec2 v_texcoord;
+
 void main() {
-    gl_FragColor = vec4(u_color, 1.0);
+    gl_FragColor = vec4(texture2D(u_roughness, v_texcoord).rgb, 1.0);
 }
 `;
