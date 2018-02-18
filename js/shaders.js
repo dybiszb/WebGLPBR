e5b1c8352638c446349b2a460d6d12b6d6acab26
dybiszb <bartlomiej.dybisz@gmail.com>
2018-02-18 16:49:02 +0100

    Dummy point light representation in form of a sphere. The light moves around the main sphere based on elapsed time.

--- js/shaders.js
+++ js/shaders.js
@@ -14,6 +14,7 @@ uniform mat4 u_view;
 uniform mat4 u_projection;
 
 varying vec2 v_texcoord;
+varying vec3 v_worldpos;
 
 void main() {
     gl_Position = u_projection *
@@ -22,6 +23,11 @@ void main() {
                   a_position;
 
     v_texcoord  = a_texcoord;
+    v_worldpos  = vec3
+                  (
+                      u_model  *
+                      a_position
+                  );
 }
 `;
 
@@ -32,10 +38,7 @@ const pbrFG =
 // =================================================
 // @author: bdybisz
 // -------------------------------------------------
-
 precision mediump float;
-
-uniform vec3 u_color;
 
 uniform sampler2D u_albedo;
 uniform sampler2D u_metallic;
@@ -43,8 +46,70 @@ uniform sampler2D u_normal;
 uniform sampler2D u_roughness;
 
 varying vec2 v_texcoord;
+varying vec3 v_worldpos;
+
+// Returns attenuation coefficient,
+// which simulates how a light source
+// intensity is dimmed over a distance.
+//
+// @param distance - Distance between fragment
+//                   position and a light source.
+//                   NOTE: Must be > 0.
+//
+// @return <0.0; 1.0> value.
+float
+calculateAttenuation
+(
+      float distance
+)
+{
+    return  1.0 / (distance * distance);
+}
 
 void main() {
     gl_FragColor = vec4(texture2D(u_roughness, v_texcoord).rgb, 1.0);
+}
+`;
+
+const lightVS =
+    `
+// =================================================
+//        L I G H T   V E R T E X   S H A D E R
+// =================================================
+// @author: bdybisz
+// -------------------------------------------------
+
+attribute vec4 a_position;
+
+uniform mat4 u_model;
+uniform mat4 u_view;
+uniform mat4 u_projection;
+
+void main() {
+    gl_Position = u_projection *
+                  u_view       *
+                  u_model      *
+                  a_position;
+}
+`;
+
+const lightFG =
+    `
+// =================================================
+//     L I G H T   F R A G M E N T   S H A D E R
+// =================================================
+// @author: bdybisz
+// -------------------------------------------------
+precision mediump float;
+
+uniform vec3 u_color;
+
+void main() {
+    vec3 scaledColor = u_color / 255.0;
+    gl_FragColor     = vec4
+                       (
+                         scaledColor,
+                         1.0
+                       );
 }
 `;
